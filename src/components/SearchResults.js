94ea9a858c52d52f34aaa295109525cc98248ca1
Jagoda Bobińska <56960044+Adenka@users.noreply.github.com>
2023-05-10 19:10:33 +0200

    Update SearchResults.js

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -4,30 +4,48 @@ import Logo from "../assets/logo2copy.svg";
 import SearchIcon from '@mui/icons-material/Search';
 import MedInfoDialog from "./MedInfoDialog";
 import SnackBar from "./SnackBar";
-import { useNavigate } from "react-router-dom";
 import { MedContext } from "./medInfo";
 import SearchField from "./utils/SearchField";
 import { requestPath } from "./utils/utils";
 
-//todo - porządne sortowanie
-
 const SearchResults = () => {
     const [meds, setMeds] = useState([]);
+    const [medsDuplicates, setMedsDuplicates] = useState([]);
 
     const [isDialogOpen, setIsDialogOpen] = useState(false);
     const [clickedMed, setClickedMed] = useState({});
-    const navigate = useNavigate();
     const {searchPhrase, setSearchPhrase} = useContext(MedContext);
     const [input, setInput] = useState("");
+    const [refunds, setRefunds] = useState([]);
     
     const getMedsLikeSearch = async () => {
-        const medsLikeSearch = await fetch(`${requestPath}/drugs?search=${searchPhrase}`, {
+        const phrase = input === "" ? searchPhrase : input;
+        const medsLikeSearch = await fetch(`${requestPath}/drugs?search=${phrase}`, {
             method: 'GET'
         });
+        
         const medsLikeSearchJson = await medsLikeSearch.json();
         const medsLikeSearchJsonSorted
-            = await medsLikeSearchJson.sort((a, b) => a.name > b.name);
-        setMeds(medsLikeSearchJson);
+            = await medsLikeSearchJson.sort(
+                (a, b) =>
+                    (a.name > b.name)
+                    || (a.name === b.name && a.formDose > b.formDose)
+                    || (a.name === b.name && a.formDose === b.formDose && a.content > b.content)
+            );
+        let medsLikeSearchJsonSortedNoDuplicates = [];
+        medsLikeSearchJsonSorted.forEach(element => {
+            let wasDuplicate = false;
+            medsLikeSearchJsonSortedNoDuplicates.forEach(element2 => {
+                if (element.name === element2.name && element.formDose === element2.formDose && element.content === element2.content) {
+                    wasDuplicate = true;
+                }
+            });
+            if (!wasDuplicate) {
+                medsLikeSearchJsonSortedNoDuplicates.push(element);
+            }
+        });
+        setMedsDuplicates(medsLikeSearchJsonSorted);
+        setMeds(medsLikeSearchJsonSortedNoDuplicates);
     }
 
     useEffect(() => {
@@ -40,12 +58,27 @@ const SearchResults = () => {
         getMedsLikeSearch();
     }
 
+    const handleOnClick = (med, key) => {
+        setIsDialogOpen(true);
+        setClickedMed({key: key, ...med})
+
+        let medRefunds = [];
+        medsDuplicates.forEach(meddd => {
+            if (med.name === meddd.name && med.formDose === meddd.formDose && med.content === meddd.content) {
+                medRefunds.push(meddd.refund);
+            }
+        });
+        
+        setRefunds(medRefunds);
+    }
+
     return (
         <div style = {{display: "flex", flexDirection: "column", width: "100vw", height: "100vh"}}>
             <MedInfoDialog
                 isOpen = {isDialogOpen}
                 setIsOpen = {setIsDialogOpen}
                 clickedMed = {clickedMed}
+                refunds={refunds}
             />
             <SnackBar/>
             <div style = {{
@@ -92,7 +125,7 @@ const SearchResults = () => {
                         }}
                         onMouseOver = {(event) => event.target.style.background = "#404040"}
                         onMouseLeave = {(event) => event.target.style.background = "none"}
-                        onClick = {() => {setIsDialogOpen(true); setClickedMed({key: key, ...med})}}
+                        onClick = {() => handleOnClick(med, key)}
                     >
                         <div style = {{pointerEvents: "none", lineHeight: 2, fontSize: 24, color: "#ffc107"}}>
                             <b>{med.name}</b>
