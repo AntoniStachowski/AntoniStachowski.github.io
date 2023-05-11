beaee35666db5915ce8bd7c2a8392b4d4c4da982
AntoniStachowski <antonistachowski@gmail.com>
2023-05-11 02:34:38 +0200

    desperate

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -28,9 +28,9 @@ const SearchResults = () => {
         const medsLikeSearchJsonSorted
             = await medsLikeSearchJson.sort(
                 (a, b) =>
-                    (a.name > b.name)
+                    {await (a.name > b.name)
                     || (a.name == b.name && a.formDose > b.formDose)
-                    || (a.name == b.name && a.formDose == b.formDose && a.content > b.content)
+                    || (a.name == b.name && a.formDose == b.formDose && a.content > b.content)}
             );
         let medsLikeSearchJsonSortedNoDuplicates = [];
         medsLikeSearchJsonSorted.forEach(element => {
